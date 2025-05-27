5fdfa1e53a15f1129a7796d8f38774cd6a88641f
FunctionJoey <functionjoey@gmail.com>
2025-05-27 17:06:33 -0600

    Linear search

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
         //priorityQueues_lesson();
         //linkedLists_lesson();
         //dynamicLists_lesson();
-        LinkedListvsArrayList_lesson();
+        //LinkedListvsArrayList_lesson();
+        linearSearch_lesson();
     }
 
     static void stack_lesson(){
@@ -396,6 +397,42 @@ public class Main {
 
     }
 
+    static void linearSearch_lesson(){
+        // Linear Search: Iterate through a collection one element at a time
+        /*
+         * runtime complexity: O(n)
+         *
+         * Disadvantages:
+         * Slow for large data sets
+         *
+         * Advantages:
+         * Fast for searches of small to medium data sets
+         * Does not need to be sorted
+         * Useful for data structures that do not have the random access (Linked List)
+         */
+
+        int[] array = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+
+        int index = linearSearch(array, 1);
+
+        if(index != -1){
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    // Intended to be used with lesson above
+    private static int linearSearch(int[] array, int value){
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == value){
+                return i;
+            }
+        }
+
+        return -1;
+    }
 
 }
 
