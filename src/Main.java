315ed6afa063e22d43b25a242e2dbf7bd65a7e97
FunctionJoey <functionjoey@gmail.com>
2025-05-28 12:46:38 -0600

    Interpolation search

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,9 @@ public class Main {
         //dynamicLists_lesson();
         //LinkedListvsArrayList_lesson();
         //linearSearch_lesson();
-        binarySearch_lesson();
+        //binarySearch_lesson();
+        //interpolationSearch_lesson();
+
     }
 
     static void stack_lesson(){
@@ -475,6 +477,55 @@ public class Main {
             else return middle; // target found
         }
         return -1; // target not found
+    }
+
+    static void interpolationSearch_lesson(){
+        //interpolation search = improvement over binary search best used for
+        //                          "uniformly" distributed "guesses" where
+        //                          a value might be based on the calculated
+        //                          probe results: if probe is incorrect,
+        //                          search area is narrowed and a new probe is
+        //                          calculated
+         /*         average case: O(log(log(n)))
+          *         worst case:   O(n) [values increase exponentially]
+          */
+
+        //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+
+        // int index = interpolationSearch(array, 8);
+        int index = interpolationSearch(array, 256);
+
+        if(index != -1){
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    // Intended to be used with lesson above (interpolation search)
+    private static int interpolationSearch(int[] array, int value) {
+
+        int high = array.length - 1;
+        int low = 0;
+
+        while(value >= array[low] && value <= array[high] && low <= high) {
+
+            int probe = low + (high - low) * (value - array[low]) /
+                        (array[high] - array[low]);
+
+            System.out.println("probe: " + probe);
+
+            if(array[probe] == value){
+                return probe;
+            } else if(array[probe] < value) {
+                low = probe + 1;
+            } else {
+                high = probe - 1;
+            }
+        }
+
+        return -1;
     }
 }
 
