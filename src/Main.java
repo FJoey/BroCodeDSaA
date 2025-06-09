da7ea53298b8c9471d59326bbbd01871c648294e
FunctionJoey <functionjoey@gmail.com>
2025-06-09 14:34:13 -0600

    Adjacency Matrix

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,8 @@ public class Main {
         //linkedLists_lesson();
         //dynamicLists_lesson();
         //LinkedListvsArrayList_lesson();
-        hashTables_lesson();
+        //hashTables_lesson();
+        adjacencyMatrix_lesson();
 
         /*SEARCH*/
         //linearSearch_lesson();
@@ -895,6 +896,38 @@ public class Main {
         }
     }
 
+    static void adjacencyMatrix_lesson(){
+        /*
+         * Adjacency Matrix = A 2D array to store 1's/0's to
+         *                    represent edges.
+         *
+         * # of rows        = # of unique nodes
+         * # of columns     = # of unique nodes
+         *
+         * runtime complexity to check an edge: O(1)
+         * space complexity:                    O(v^2) [v = vertices]
+         */
+
+        Graph graph = new Graph(5);
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.print();
+
+        System.out.println(graph.checkEdge(0, 1));
+        System.out.println(graph.checkEdge(3, 2));
+    }
 }
 
 
